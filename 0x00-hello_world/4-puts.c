36502b34d17f1c9319fa04e0bf5634d73de1d6f4
Applam <oyewumidavid833@gmail.com>
2023-09-07 21:06:59 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - this is a prinf function
+ * main - this is the main put function
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
 
